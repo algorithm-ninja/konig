6bc30fefee460c0d7a4a05b628211db2ca52b977
Gabriele Farina <gabr.farina@gmail.com>
2015-05-03 15:33:04 +0200

    Some code documentation

--- konig/include/AdjacencyManager.hpp
+++ konig/include/AdjacencyManager.hpp
@@ -14,15 +14,17 @@ namespace konig {
 
         // Defined types
     public:
+        typedef std::pair<vid_t, vid_t> adjacency_endpoints_t;
+
         struct adjacency_t {
-            vid_t first, second;
+            adjacency_endpoints_t endpoints;
             weight_t weight;
 
-            adjacency_t(vid_t first, vid_t second, weight_t weight = weight_t())
-                    : first(first), second(second), weight(weight) { };
+            adjacency_t(adjacency_endpoints_t endpoints, weight_t weight)
+                    : endpoints(endpoints), weight(weight) { }
 
             bool operator<(const adjacency_t& other) const {
-                return (first < other.first) || (first == other.first && second < other.second);
+                return endpoints < other.endpoints;
             }
         };
 
@@ -49,12 +51,15 @@ namespace konig {
 
     public:
         /**
-         * @brief Iterator object over the adjacencies.
+         * @brief Iterator over the (weighted) adjacencies.
          *
          */
         class iterator : std::iterator<std::random_access_iterator_tag, adjacency_t> {
         private:
             SplayNode* splay_node;
+        public:
+            iterator(SplayNode* splay_node)
+                    : splay_node(splay_node) { }
         };
 
         // Members
@@ -67,38 +72,130 @@ namespace konig {
 
         // Methods
     public:
-        void insert(const vid_t first, const vid_t second, const weight_t weight) {
+
+        /**
+         * @brief Inserts adjacency in the AdjacencyManager.
+         *
+         * @param w_adjacency A weighted adjacency
+         */
+        void insert(const adjacency_t adjacency) {
 
         }
 
-        void erase(const vid_t first, const vid_t second) {
+        /**
+         * @brief Deletes adjacency in the AdjacencyManager, given its endpoints.
+         *
+         * @param endpoints The endpoints of the adjacency to be deleted
+         */
+        void erase(const adjacency_endpoints_t endpoints) {
 
         }
 
-        // NOTE: find, lower_bound, ... cannot be declared as const, since the potentially
-        // update the splay tree root.
-        iterator find(const vid_t first, const vid_t second) noexcept {
+        /**
+         * @brief Deletes adjacency, given an adjacency_t object.
+         *
+         * This is just a handy wrapper for erase(const adjacency_endpoints_t).
+         * @warning This function completely ignores the weight field.
+         *
+         * @param adjacency A weighted adjacency object
+         */
+        void erase(const adjacency_t adjacency) {
+            erase(adjacency.endpoints);
+        }
+
+        /**
+         * @brief Deletes adjacency in the AdjacencyManager, given a valid iterator.
+         *
+         * @param iter Iterator to the adjacency to be deleted
+         */
+        void erase(const iterator iter) {
+            erase(iter->endpoints);
+        }
+
+        /**
+         * @brief Returns an iterator to the first adjacency coming after the provided one, according to
+         *        standard increasing lexicographical ordering.
+         *
+         * @param endpoints The endpoints of the adjacency
+         */
+        iterator lower_bound(const adjacency_endpoints_t endpoints) noexcept {
 
         }
 
-        iterator lower_bound(const vid_t first, const vid_t second) noexcept {
-
+        /**
+         * @brief Returns an iterator to the first adjacency coming after the provided one, according to
+         *        standard increasing lexicographical ordering.
+         *
+         * This is just a handy wrapper for lower_bound(const adjacency_endpoints_t).
+         * @warning This function completely ignores the weight field.
+         *
+         * @param adjacency A weighted adjacency object
+         */
+        iterator lower_bound(const adjacency_t adjacency) noexcept {
+            return lower_bound(adjacency.endpoints);
         }
 
+        /**
+         * @brief Returns an iterator to the adjacency having the provided endpoints.
+         *
+         * If no stored adjacency matches the provided endpoints, this->end() will be returned.
+         *
+         * @param iter Iterator to the adjacency to be deleted
+         */
+        iterator find(const adjacency_endpoints_t endpoints) noexcept {
+            auto ans = lower_bound(endpoints);
+            if (ans == end() || ans->endpoints != endpoints)
+                return end();
+            else
+                return ans;
+        }
+
+        /**
+         * @brief Returns an iterator to the adjacency having the provided endpoints.
+         *
+         * This is just a handy wrapper for find(const adjacency_endpoints_t).
+         * @warning This function completely ignores the weight field.
+         *
+         * @param iter Iterator to the adjacency to be deleted
+         */
+        iterator find(const adjacency_t adjacency) noexcept {
+            return find(adjacency.endpoints);
+        }
+
+        /**
+         * @brief Returns an iterator to the first adjacency.
+         */
         iterator begin() const noexcept {
-
+            return begin(0);
         }
 
+        /**
+         * @brief Returns an iterator to the first adjacency having a given first endpoint.
+         *
+         * @param first The first endpoint
+         */
         iterator begin(const vid_t first) const noexcept {
-
+            return vertex_lower_bound[first];
         }
 
+        /**
+         * @brief Returns an iterator referring to the past-the-end element.
+         */
         iterator end() const noexcept {
-
+            return end(n_vertices - 1);
         }
 
+        /**
+         * @brief Returns an iterator referring to the past-the-end element in the range of adjacencies having
+         *        a given first endpoint.
+         *
+         * @param first The first endpoint
+         */
         iterator end(const vid_t first) const noexcept {
-
+            if (first < n_vertices - 1)
+                return vertex_lower_bound[first + 1];
+            else
+                return NULL;
         }
 
         iterator kth_present(const size_t k) noexcept {
